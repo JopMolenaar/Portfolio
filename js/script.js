0e8d91a2ad99f451ab1809b2e5834d811d00e91f
JopMolenaar <112875494+JopMolenaar@users.noreply.github.com>
2022-12-01 10:55:19 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log("hoi")
+
+
+
+// const cursor = document.querySelector("div")
+
+
+
+// Bron https://www.youtube.com/watch?v=rfpRZ2t_BrQ
+// document.addEventListener("mousemove", e => {
+//     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;");
+
+
+//     document.body.append(div)
+//     setTimeout(() => {
+//         cursor.remove()
+//     },900)
+// });
+
+
+
+// 
