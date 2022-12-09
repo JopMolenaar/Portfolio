b509d22b35f432f551397ffc85ef0053767d4eee
Jop Molenaar <112875494+JopMolenaar@users.noreply.github.com>
2022-12-09 12:34:41 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("hoi")
+
+var cursor = document.querySelector("body > div");
+var speelveld = document.querySelector("body");
+
+
+speelveld.addEventListener("mousemove", e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 150)+"px; left: "+(e.pageX - 150)+"px;");
+});
