173ab7a8ee40bad4fdae68349564cf2d6aab16e6
Jop <jopmolenaar@icloud.com>
2024-01-05 12:23:54 +0100

    header scroll animation

--- static/js/script.js
+++ static/js/script.js
@@ -48,3 +48,68 @@
 //   },
 //   false
 // );
+
+// // Select the image element
+// const image = document.querySelector("div:nth-of-type(2)");
+// const list = document.querySelector("main > div:first-child");
+// // Set the scroll speed ratio for the image (50% slower)
+// const scrollSpeedRatio = 0.5;
+
+// // Get the initial scroll position
+// let previousScrollPosition = list.scrollTop;
+
+// // Function to handle the scroll event
+// const handleScroll = () => {
+//     // console.log(list.scrollTop);
+//     // Calculate the vertical scroll distance
+//     const scrollDistance = list.scrollTop - previousScrollPosition;
+//     console.log(image.offsetTop);
+//     // Calculate the new image position based on the scroll distance and speed ratio
+//     const newPosition = scrollDistance * scrollSpeedRatio;
+//     // Update the image position
+//     if (newPosition < 0) {
+//         image.style.transform = `translateY(0px)`;
+//     } else {
+//         image.style.transform = `translateY(-300px)`;
+//     }
+//     // image.style.top = `${newPosition}px`;
+//     // console.log(newPosition, scrollDistance);
+//     // Store the current scroll position for the next scroll event
+//     previousScrollPosition = list.scrollTop;
+
+//     // console.log(newPosition);
+// };
+
+// // Attach the scroll event listener to the window object
+// list.addEventListener("scroll", handleScroll);
+
+// Select the image element
+const image = document.querySelector("div:nth-of-type(2)");
+const list = document.querySelector("main > div:first-child");
+// Set the scroll speed ratio for the image (50% slower)
+const scrollSpeedRatio = 0.5;
+let previousScrollPosition = list.scrollTop;
+let newPosition = 0;
+let prevNewPosition = 0;
+const maxTranslate = 300;
+// Function to handle the scroll event
+const handleScroll = () => {
+    const scrollDistance = list.scrollTop - previousScrollPosition;
+    // Calculate the new image position based on the scroll distance and speed ratio
+    newPosition = newPosition + scrollDistance;
+
+    if (newPosition < prevNewPosition) {
+        image.style.transform = `translateY(0px)`;
+    } else {
+        if (newPosition > maxTranslate) {
+            image.style.transform = `translateY(-${maxTranslate}px)`;
+        } else {
+            image.style.transform = `translateY(-${newPosition}px)`;
+        }
+    }
+    prevNewPosition = newPosition;
+    previousScrollPosition = list.scrollTop;
+};
+
+// Attach the scroll event listener to the window object
+list.addEventListener("scroll", handleScroll);
